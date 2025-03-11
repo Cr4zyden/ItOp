0788a2de0338100227a67380e8da232c9680b0a1
Cr4zyden <denrus2006@gmial.com>
2025-03-11 09:47:25 +0700

    made admission for second lab

--- Sem_2/Lab_2/admission.cpp
+++ Sem_2/Lab_2/admission.cpp
@@ -2,57 +2,76 @@
 #include <fstream>
 #include <string>
 
-struct Stack
+struct queue
 {    
-    int top;
-    char arr[100];
+    int beg = 0;
+    int end = -1;
+    int size = 100;
+    int vals[100] = {};
 };
 
-void push(Stack*& stack, char val){
-    if(stack->top >= 100 - 1){
-        //std::cout<<"Stack overflow"<<std::endl;
-        return;
-    }
-    stack->top++;
-    stack->arr[stack->top] = val;
+void push(queue* q, int val) {
+    if (q->end >= q->size - 1) return; 
+    q->end++;
+    q->vals[q->end] = val;
 }
 
-void print(Stack* stack){
-    while (stack->top >=0)
-    {
-        std::cout<<stack->arr[stack->top]<<std::endl;
-        stack->top--;
-    }
+void pop(queue* q) {
+    if (q->beg > q->end) return; 
+    std::cout << q->vals[q->beg] << std::endl;
+    q->beg++;
 }
 
-char pop(Stack*& stack) {
-    if (stack->top < 0) {
-        std::cout << "Stack is empty\n";
-        // throw std::underflow_error("Stack is empty");
-        return -1;  // Return -1 or handle underflow differently
-    } else {
-        stack->top--;
-        return stack->arr[stack->top+1];  // Return the top item and decrement top
-    }
-}
+int main() {
+    bool sign_flag = false;
+    int num;
 
-int main(){
-    Stack* f;
-    f->top = -1;
-    char last = NULL;
+    queue* q = new queue();
+
     std::ifstream file("data.txt");
+    if (!file.is_open()) {
+        std::cerr << "Ошибка открытия файла!" << std::endl;
+        return 1;
+    }
+
     std::string temp_str;
-    std::getline(file, temp_str);
-    for(char elem: temp_str){
-        if(elem == ' ') continue;
-        if(elem == '&' or elem == '|'){ 
-            last = pop(f);
-            push(f, elem);
+    
+    while (std::getline(file, temp_str)) {
+        for (char elem : temp_str) {
+            if (elem == ' ') continue;
+            if (elem == '-') {
+                sign_flag = true;
+                continue;
+            }
+            if (elem == '0') {
+                break;
+            }
+            if (std::isdigit(elem)) {
+                num = elem - '0';
+                if (sign_flag) {
+                    num *= -1;
+                    sign_flag = false;
+                }
+                push(q, num);
+            }
         }
-        else{
-        
+
+    }
+
+    std::cout << "Отрицательные элементы:" << std::endl;
+    for (int i = q->beg; i <= q->end; i++) {
+        if (q->vals[i] < 0) {
+            std::cout << q->vals[i] << std::endl;
         }
     }
-    print(f);
 
+    std::cout << "Положительные элементы:" << std::endl;
+    for (int i = q->beg; i <= q->end; i++) {
+        if (q->vals[i] >= 0) {
+            std::cout << q->vals[i] << std::endl;
+        }
+    }
+    delete q;
+
+    return 0;
 }
